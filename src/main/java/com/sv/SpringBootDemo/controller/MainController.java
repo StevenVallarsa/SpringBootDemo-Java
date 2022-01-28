08e6096c3f361549a47988311fededa3fab92f66
StevenVallarsa <orionsjv@gmail.com>
2022-01-27 22:08:51 -0500

    first commit

---
+++ src/main/java/com/sv/SpringBootDemo/controller/MainController.java
@@ -0,0 +1,13 @@
+
+package com.sv.SpringBootDemo.controller;
+
+/**
+ *
+ * @author: Steven Vallarsa
+ *   email: stevenvallarsa@gmail.com
+ *    date: 2022-01-27
+ * purpose: 
+ */
+public class MainController {
+
+}
