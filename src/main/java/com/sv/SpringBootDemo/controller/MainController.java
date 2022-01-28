f35ddbfac1a81612e71f8979b59cb10dafe497de
StevenVallarsa <orionsjv@gmail.com>
2022-01-28 13:00:42 -0500

    explored some Thymeleaf posibilities

--- src/main/java/com/sv/SpringBootDemo/controller/MainController.java
+++ src/main/java/com/sv/SpringBootDemo/controller/MainController.java
@@ -1,6 +1,8 @@
 
 package com.sv.SpringBootDemo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,7 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class MainController {
     
-    String name = "Steve";
+    String name = "<h1>BAD CODE</h1>";
     int number = 99;
 
     @GetMapping("test")
@@ -33,5 +35,36 @@ public class MainController {
         name = request.getParameter("formName");
         number = Integer.parseInt(request.getParameter("formNumber"));
         return "redirect:/test";
+    }
+    
+    @GetMapping("testList")
+    public String testList(Model model) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(10);
+        numbers.add(100);
+        numbers.add(1000);
+        numbers.add(10000);
+        
+        model.addAttribute("numbers", numbers);
+        
+        return "testList";
+    }
+    
+    @GetMapping("testConditional")
+    public String testConditional(Model model) {
+        
+        model.addAttribute("truth", true);
+        model.addAttribute("number", number);
+        model.addAttribute("aString", "differentCase2");
+        
+        return "testConditional";
+    }
+    
+    @PostMapping("upDown")
+    public String upDown(HttpServletRequest request) {
+        if (request.getParameter("down") == null) number++;
+        else number--;
+        return "redirect:/testConditional";
     }
 }
